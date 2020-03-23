b7145fe0b92ab954380b514680dc5f0c432848c8
Eugene K <eugene.kobyakov@netfoundry.io>
2020-03-23 12:54:41 -0400

    implement ziti

--- android/src/main/java/io/netfoundry/ziti/rn/RNZitiModule.java
+++ android/src/main/java/io/netfoundry/ziti/rn/RNZitiModule.java
@@ -5,6 +5,24 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.modules.network.OkHttpClientFactory;
+import com.facebook.react.modules.network.OkHttpClientProvider;
+import com.facebook.react.modules.network.ReactCookieJarContainer;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.security.KeyStore;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
+
+import io.netfoundry.ziti.Ziti;
+import okhttp3.Dns;
+import okhttp3.OkHttpClient;
 
 public class RNZitiModule extends ReactContextBaseJavaModule {
 
@@ -13,10 +31,51 @@ public class RNZitiModule extends ReactContextBaseJavaModule {
   public RNZitiModule(ReactApplicationContext reactContext) {
     super(reactContext);
     this.reactContext = reactContext;
+
+    OkHttpClientProvider.setOkHttpClientFactory(zitiClientFactory);
   }
 
   @Override
   public String getName() {
     return "RNZiti";
   }
+
+  private final Dns zitiDns = new Dns(){
+    @Override
+    public List<InetAddress> lookup(String hostname) throws UnknownHostException {
+      InetAddress address = Ziti.getDNSResolver().resolve(hostname);
+      if (address == null) {
+        address = InetAddress.getByName(hostname);
+      }
+
+      return address != null ? Collections.singletonList(address)
+              : Collections.<InetAddress>emptyList();
+    }
+  };
+
+  private final OkHttpClientFactory zitiClientFactory = new OkHttpClientFactory() {
+    @Override
+    public OkHttpClient createNewNetworkModuleClient() {
+      SSLSocketFactory sslSocketFactory = Ziti.getSSLSocketFactory();
+      X509TrustManager tm = null;
+      try {
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        tmf.init((KeyStore) null);
+        tm = (X509TrustManager) tmf.getTrustManagers()[0];
+      } catch (Exception ex) {
+
+      }
+      OkHttpClient.Builder builder = new OkHttpClient.Builder()
+              .connectTimeout(0, TimeUnit.MILLISECONDS)
+              .readTimeout(0, TimeUnit.MILLISECONDS)
+              .writeTimeout(0, TimeUnit.MILLISECONDS)
+              .socketFactory(Ziti.getSocketFactory())
+              .sslSocketFactory(sslSocketFactory, tm)
+              .dns(zitiDns)
+              .cookieJar(new ReactCookieJarContainer());
+
+      return builder.build();
+    }
+  };
+
 }
--- android/src/main/java/io/netfoundry/ziti/rn/RNZitiPackage.java
+++ android/src/main/java/io/netfoundry/ziti/rn/RNZitiPackage.java
@@ -10,10 +10,17 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
+
+import io.netfoundry.ziti.android.Ziti;
+
 public class RNZitiPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new RNZitiModule(reactContext));
+        // we have to use seamless mode here to make React Native websockets module work
+        // RN websockets does not use client factory override
+        Ziti.init(reactContext.getApplicationContext(), true);
+
+        return Arrays.<NativeModule>asList(new RNZitiModule(reactContext));
     }
 
     // Deprecated from RN 0.47
